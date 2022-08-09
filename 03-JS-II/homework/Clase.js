b7a3e402a47090a03988ab6532df24c14da3ce51
LuisD182 <ld_222@hotmail.coom>
2022-08-08 20:02:31 -0500

    Tarea bien hecha JS III

--- 03-JS-II/homework/Clase.js
+++ 03-JS-II/homework/Clase.js
@@ -1,5 +1,10 @@
-if ("") {
+if ("10") {
     console.log("Este codigo se ejecuta si es true");
 } else {
     console.log("Este codigo se ejecuta si es false");
 }
+function imprime1aN(n) {
+for (var i = 1 ; i <=n; 1++ ) 
+console.log(i);
+}
+return i
